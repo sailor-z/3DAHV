35a51d1c5dfb9ccde66376eec202c1859519270c
Sailor-z <sailorzzcc@gmail.com>
2024-01-22 10:51:44 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-# CUDA_VISIBLE_DEVICES=0 python train_estimator.py
-# python train_estimator_pl.py
-# python train_regressor_objaverse.py
-
-for i in $(seq 1 1 50)
-do
-   python test_mv_co3d.py
-done
