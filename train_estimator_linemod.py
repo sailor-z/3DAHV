c83b7ab0d82a65fa4e96965f372918cb421bd865
Sailor-z <sailorzzcc@gmail.com>
2024-04-01 12:06:31 +0200

    Update train_estimator_linemod.py

--- train_estimator_linemod.py
+++ train_estimator_linemod.py
@@ -1,21 +1,16 @@
 from types import SimpleNamespace
-import wandb
 import yaml
 import os
 from lightning.pytorch.callbacks import ModelCheckpoint, StochasticWeightAveraging
 import lightning.pytorch as pl
 from utils import mk_folders
-from modules.PL_delta_rota_att_mask_tiny import training_lm, Estimator
-# from modules.PL_delta_rota_att_mask_tiny_3d import training_lm, Estimator
-# from modules.PL_delta_rota_att_mask_wo_att_tiny import training_lm, Estimator
-# from modules.PL_delta_rota_att_mask_img_tiny import training_lm, Estimator
-
+from modules.model import training_lm, Estimator
 
 def main(cfg):
-    cfg["RUN_NAME"] = 'Objaverse_delta_rota_att_mask'
+    cfg["RUN_NAME"] = 'LINEMOD_3DAHV'
     cfg["DATA"]["ACC_THR"] = 15
-    cfg["DATA"]["NUM_ROTA"] = 9000 #15000
-    cfg["TRAIN"]["BS"] = 24#12
+    cfg["DATA"]["NUM_ROTA"] = 9000
+    cfg["TRAIN"]["BS"] = 12
     cfg["TRAIN"]["LR"] = 1e-5
     cfg["TRAIN"]["MAX_EPOCH"] = 10
     cfg["DATA"]["BG"] = True
@@ -24,33 +19,13 @@ def main(cfg):
     cfg["TRAIN"]["MASK"] = True
     cfg["TRAIN"]["MASK_RATIO"] = 0.25
 
-    if cfg["RUN_NAME"] == 'Objaverse_delta_rota_att_wo_mask':
-        cfg["TRAIN"]["MASK"] = False
-        cfg["TRAIN"]["MASK_RATIO"] = 0.0
-
-    # ##LINEMOD
-    # cfg["LINEMOD"]["OCC"] = False
-
-    ##LINEMOD-O
-    cfg["LINEMOD"]["OCC"] = True
-
     print(cfg)
 
-    if cfg["LINEMOD"]["OCC"] == False:
-        filename = "checkpoint_lm"
-    else:
-        filename = "checkpoint_lm_occ"
-
-    checkpoint_callback = ModelCheckpoint(monitor='train_loss', mode='min', dirpath=os.path.join("./models", cfg["RUN_NAME"]), filename=filename)
+    checkpoint_callback = ModelCheckpoint(monitor='train_loss', mode='min', dirpath=os.path.join("./models", cfg["RUN_NAME"]), filename="checkpoint_lm")
 
     trainer = pl.Trainer(accelerator="auto", devices="auto", strategy="ddp_find_unused_parameters_true", accumulate_grad_batches=1,
         max_epochs=cfg["TRAIN"]["MAX_EPOCH"], sync_batchnorm=True, limit_train_batches=cfg["TRAIN"]["SAMPLE_RATE"],
         callbacks=[checkpoint_callback])
-
-    if trainer.global_rank == 0:
-        mk_folders(cfg["RUN_NAME"])
-        wandb.login(key='b57ba9e1ca7deed19c1ee2b694c16b1744807f4f')
-        wandb.init(project="train_delta_rota_att_mask_lm", group="train")
 
     training_lm(cfg, trainer)
 
